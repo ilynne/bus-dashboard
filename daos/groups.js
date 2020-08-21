a23a9e91a1d34f54831d61e683ed36c0ca68fcd8
Sven Otto <svenotto@uw.edu>
2020-08-20 19:23:50 -0700

    inital modal and routes, some doas remaining

--- daos/groups.js
+++ daos/groups.js
@@ -4,4 +4,45 @@ const Group = require('../models/group');
 
 module.exports = {};
 
+module.exports.create = async(userId, origin, destination, isDefault) => {
+    try {
+        const newGroup = await Group.create({
+            userId: userId,
+            origin: origin,
+            destination: destination,
+            isDefault: isDefault
+        });
+        return newGroup
+    } catch (e) {
+        throw e;
+    }
+};
+
+module.exports.getByOrigin = async () => {};
+
+module.exports.getByDestination = async () => {};
+
+module.exports.getByOriginAndDestination = async () => {};
+
+module.exports.getByOriginAndDestination = async () => {};
+
+module.exports.getByUserId = async () => {};
+
+module.exports.getAssociatedUserId = async () => {};
+
+module.exports.getById = async () => {};
+
+module.exports.updateById = async () => {};
+
+module.exports.deleteById = async (groupId) => {
+    const validId = await mongoose.Types.ObjectId.isValid(groupId);
+    try{
+        if (validId) {
+            await Group.remove({ _id: groupId })
+        }
+    } catch (e) {
+        throw e;
+    }
+}
+
 // DAO operations
--- routes/groups.js
+++ routes/groups.js
@@ -1,26 +1,122 @@
-const { Router, query } = require("express");
+const { Router, query, response } = require("express");
+const router = Router();
+
+const groupDAO = require('../daos/groups');
 
 // Authentication middleware
 
 // Routes
-router.post("/", async (req, res, next) => {
-    // to complete
-});
+router.post("/",
+    // authentication middleware, 
+    async (req, res, next) => {
+        const userId = req.user._id;
+        const { origin, destination, isDefault } = req.body;
+        const newGroup = await groupDAO.create(userId, origin, destination, isDefault);
+        if (newGroup) {
+            res.json(newGroup);
+        } else {
+            res.sendStatus(404);
+        }
+    }
+);
 
-router.get("/", async (req, res, next) => {
-    // to complete
-});
+router.get("/", 
+    // authentication middleware, 
+    async (req, res, next) => {
+        let { origin, destination } = req.query;
+        if (origin) {
+            const groups = await groupDAO.getByOrigin(origin);
+            if (groups) {
+                res.json(groups);
+            } else {
+                res.sendStatus(404);
+            }
+        } else if (destination) {
+            const groups = await groupDAO.getByDestination(destination);
+            if (groups) {
+                res.json(groups);
+            } else {
+                res.sendStatus(404);
+            }
+        } else if (origin && destination) {
+            const groups = await groupDAO.getByOriginAndDestination(origin, destination);
+            if (groups) {
+                res.json(groups);
+            } else {
+                res.sendStatus(404);
+            }
+        } else {
+            const userId = req.user._id;
+            const groups = await groupDAO.getByUserId(userId);
+            if (groups) {
+                res.json(groups);
+            } else {
+                res.sendStatus(404);
+            }
+        }
+    }
+);
 
-router.get("/:id", async (req, res, next) => {
-    // to complete
-});
+router.get("/:id",
+    // authentication middleware, 
+    async (req, res, next) => {
+        const groupId = req.params.id;
+        if (groupId) {
+            const userId = req.user._id;
+            const storedUserId = await groupDAO.getAssociatedUserId(userId);
+            if (userId == storedUserId) {
+                const group = await groupDAO.getById(groupId);
+                res.json(group)
+            } else {
+                res.sendStatus(404);
+            }
+        } else {
+            res.sendStatus(404);
+        }
+    }
+);
 
-router.put("/:id", async (req, res, next) => {
-    // to complete
-});
+router.put("/:id",
+    // authentication middleware, 
+    async (req, res, next) => {
+        const groupId = req.params.id;
+        if (groupId) {
+            const userId = req.user._id;
+            const storedUserId = await groupDAO.getAssociatedUserId(userId);
+            if (userId == storedUserId) {
+                const { origin, destination, isDefault } = req.body;
+                const updatedGroup = await groupDAO.updateById(groupId, origin, destination, isDefault)
+                if (updatedGroup) {
+                    res.json(updatedGroup);
+                } else {
+                    res.sendStatus(404);
+                }
+            } else {
+                res.sendStatus(404);
+            }       
+        } else {
+            res.sendStatus(404);
+        }
+    }
+);
 
-router.delete("/:id", async (req, res, next) => {
-    // to complete
-});
+router.delete("/:id",
+    // authentication middleware, 
+    async (req, res, next) => {
+        const groupId = req.params.id;
+        if (groupId) {
+            const userId = req.user._id;
+            const storedUserId = await groupDAO.getAssociatedUserId(userId);
+            if (userId == storedUserId) {
+                await groupDAO.deleteById(groupId);
+                res.sendStatus(200);
+            } else {
+                res.sendStatus(404);
+            }            
+        } else {
+            res.sendStatus(404);
+        }
+    }
+);
 
 module.exports = router;
