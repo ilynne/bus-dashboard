60b34f9f32afe70d74f508aea12d2ac8b285478f
Sven Otto <svenotto@uw.edu>
2020-08-22 17:01:20 -0700

    manual update to latest develop branch because of rebase errors

--- daos/user.js
+++ daos/user.js
@@ -1,7 +1,34 @@
-const mongoose = require('mongoose');
-
 const User = require('../models/user');
 
 module.exports = {};
 
-// DAO operations
+module.exports.createUser = async (userObj) => {
+  try {
+    const user = await User.create(userObj);
+    return user;
+  } catch(e) {
+    if (e.message.includes('validation failed')) {
+      throw new BadDataError(e.message);
+    }
+    throw e;
+  }
+}
+
+module.exports.getUser = async (email) => {
+  try {
+    return await User.findOne({ email: email }).lean();
+  } catch(e) {
+    throw e;
+  }
+}
+
+module.exports.updateUserPassword = async (userId, password) => {
+  try {
+    return await User.update({ _id: userId }, { password: password })
+  } catch(e) {
+    throw e;
+  }
+}
+
+class BadDataError extends Error {};
+module.exports.BadDataError = BadDataError;
