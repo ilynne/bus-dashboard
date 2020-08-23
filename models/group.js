f17345be11b86585264b28bc5723f6a998e16481
Sven Otto <svenotto@uw.edu>
2020-08-23 14:26:38 -0700

    resolving conflicts

--- models/group.js
+++ models/group.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
 const groupSchema = new mongoose.Schema({
-    // to complete
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'users', required: true },
+    origin: { type: String, required: true },
+    destination: { type: String, required: true },
+    isDefault: { type: Boolean, default: false, required: true}
 });
 
 module.exports = mongoose.model("groups", groupSchema);
