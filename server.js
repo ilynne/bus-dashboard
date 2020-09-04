d713255da6845df13546eb2658ea670e05d6b284
Lynne <ilynne@gmail.com>
2020-09-03 22:57:48 -0700

    adds static route to react front end

--- server.js
+++ server.js
@@ -3,10 +3,20 @@ const express = require("express");
 const routes = require("./routes");
 
 const server = express();
+
 server.use(express.json());
 
 server.use(routes);
 
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  server.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  server.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 module.exports = server;
-
-
